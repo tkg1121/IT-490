6f9822d094d0275de1bc3be0c58d213feb3261c3
AshleyS <as432@njit.edu>
2024-10-24 14:28:13 -0400

    webpage building

--- frontend/index.php
+++ frontend/index.php
@@ -1,85 +1,128 @@
 <?php
 session_start();
+include 'header.php';
 ?>
 <!DOCTYPE html>
-<html data-bs-theme="light" lang="en" style>
-
+<html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
-    <title>IT490</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f3e4d0;
+        }
+
+        .login-container {
+            width: 100%;
+            height: 90vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #f3e4d0;
+        }
+
+        .login-box {
+            background-color: #fff;
+            padding: 90px;
+            border-radius: 30px;
+            box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+
+        .login-box h2 {
+            margin-bottom: 20px;
+            font-size: 24px;
+            color: #4d3b26;
+        }
+
+        .login-box .input-box {
+            margin-bottom: 20px;
+        }
+
+        .login-box .input-box input {
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            font-size: 16px;
+        }
+
+        .login-box button {
+            background-color: #80592d;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        .login-box button:hover {
+            background-color: #69471d;
+        }
+
+        .login-box a {
+            color: #80592d;
+            text-decoration: none;
+        }
+
+        .login-box .sign-up {
+            margin-top: 10px;
+        }
+
+        .top-bar {
+            background-color: #80592d;
+            color: white;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px 20px;
+            font-size: 16px;
+        }
+
+  
+
+        .logo img {
+            height: 50px; /* Adjust size as needed */
+        }
+    </style>
 </head>
-
 <body>
-    <nav class="navbar navbar-expand-md py-3" data-bs-theme="dark" style="background: #795833;color: #795833;border-style: none;border-color: #795833;border-top-style: none;border-right-style: none;border-bottom-style: none;border-left-style: none;">
-        <div class="container">
-            <a class="navbar-brand d-flex align-items-center" href="index.html">
-                <span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon" style="background: url('C.png');">
-                    <img width="134" height="186" style="padding-top: 26px;padding-bottom: 26px;margin-bottom: -33px;margin-top: 1px;padding-right: 0px;margin-right: -11px;padding-left: 0px;margin-left: -89px;" src="C.png" loading="lazy" />
-                </span>
-            </a>
-            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-5">
-                <span class="visually-hidden">Toggle navigation</span>
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div id="navcol-5" class="collapse navbar-collapse" style="background: #795833;">
-                <ul class="navbar-nav ms-auto" style="background: #795833;">
-                    <?php if (isset($_SESSION['username'])): ?>
-                        <li class="nav-item">
-                            <span class="nav-link">Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</span>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="logout.php">Log out</a></li>
-                    <?php else: ?>
-                        <li class="nav-item"><a class="nav-link active" href="login.html">Login Page</a></li>
-                        <li class="nav-item"><a class="nav-link" href="signup.html">Sign Up</a></li>
-                    <?php endif; ?>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    
-    <main></main>
-    
-    <section class="position-relative py-4 py-xl-5" style="background: #f0dfc8;">
-        <div class="container">
-            <div class="row mb-5">
-                <div class="col-md-8 col-xl-6 text-center mx-auto">
-                    <?php if (isset($_SESSION['username'])): ?>
-                        <h2>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h2>
-                    <?php else: ?>
-                        <h2>Sign up page!</h2>
-                    <?php endif; ?>
-                </div>
-            </div>
-            
-            <div class="row d-flex justify-content-center">
-                <div class="col-md-6 col-xl-4">
-                    <div class="card mb-5">
-                        <div class="card-body d-flex flex-column align-items-center" style="margin-top: -25px;">
-                            <div class="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4" style="background: #795833;">
-                                <svg class="bi bi-person" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z"></path>
-                                </svg>
-                            </div>
-                            <?php if (!isset($_SESSION['username'])): ?>
-                                <form class="text-center" method="post">
-                                    <div class="mb-3"><input class="form-control" type="email" name="Username" placeholder="Username" /></div>
-                                    <div class="mb-3">
-                                        <input class="form-control" type="password" name="password" placeholder="Password" style="margin-bottom: 14px;" />
-                                        <input class="form-control" type="text" name="password" placeholder="Password" />
-                                    </div>
-                                    <div class="mb-3">
-                                        <button class="btn btn-primary d-block w-100" type="submit" style="background: #795833;">Sign Up</button>
-                                    </div>
-                                </form>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</body>
 
+
+
+    <div class="login-container">
+        <div class="login-box">
+            <?php
+            if (isset($_SESSION['username'])) {
+                echo "<h2>Welcome, " . htmlspecialchars($_SESSION['username']) . "!</h2>";
+                echo "<p><a href='logout.php'>Log out</a></p>";
+            } else {
+                echo '<h1>Log in page!</h1>';
+                echo '
+                <div class="input-box">
+                    <input type="text" placeholder="Username" required>
+                </div>
+                <div class="input-box">
+                    <input type="password" placeholder="Password" required>
+                </div>
+                <button>Login</button>
+                
+                <div class="sign-up">
+                    <p>Don\'t have an account with us?</p>
+                    <a href="signup.html"><button>Sign Up</button></a>
+                </div>';
+            }
+            ?>
+        </div>
+    </div>
+
+</body>
 </html>
+
 
