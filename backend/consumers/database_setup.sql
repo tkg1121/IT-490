20aa661eac3338bdce0785aa1cee8990b4f0e303
tkg1121 <tkg@njit.edu>
2024-11-27 13:32:33 -0500

    SQL logic fixed for deployment

--- backend/consumers/database_setup.sql
+++ backend/consumers/database_setup.sql
@@ -1,7 +1,8 @@
 -- Drop existing databases if they exist
 DROP DATABASE IF EXISTS social_media;
-DROP DATABASE IF EXISTS user_auth;
 DROP DATABASE IF EXISTS movie_reviews_db;
+DROP DATABASE IF EXISTS user_auth;
+
 
 -- Create the user_auth database
 CREATE DATABASE IF NOT EXISTS user_auth;
